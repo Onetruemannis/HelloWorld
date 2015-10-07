d892821d455bf4cfad67a8df80b874c48ddcdcba
Onetruemannis <ryan.couvrette@ocsbstudent.ca>
2015-10-07 14:25:41 -0400

    The first commit

---
+++ Hello World/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Hello_World
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(103, 109);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(62, 13);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.Text = "Hello World";
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Name = "frmHelloWorld";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
